9cbd0a235e387e25c210bc002f1e3896963d01e3
Robert Knight <robertknight@gmail.com>
2019-03-22 00:56:55 +0000

    Make the adapter class a default export
    When the export name was changed to `Adapter` the intent was to make it
    a drop-in replacement for the "enzyme-adapter-react-*" packages. However
    those packages export the adapter as a _default_ export, which is
    typically named `Adapter` by the consumer.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Adapter from './Adapter';
 
 export {
+  // Non-default exports for backwards compatibility with earlier v1.x releases.
   Adapter,
-  // Alias for backwards compatibility with earlier v1.x releases.
   Adapter as PreactAdapter,
 };
+
+export default Adapter;
