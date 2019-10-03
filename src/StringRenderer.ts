5d29198e605730ba0359e8848876866973739735
Robert Knight <robertknight@gmail.com>
2019-10-03 08:52:27 +0100

    Update a comment about preact-render-to-string and Preact v10

--- src/StringRenderer.ts
+++ src/StringRenderer.ts
@@ -6,8 +6,11 @@ import { isPreact10 } from './util';
 
 export default class StringRenderer implements Renderer {
   render(el: JSXElement, context?: any) {
+    // FIXME - The behavior here is different across different Preact versions.
+    // Historically this was because preact-render-to-string v4.x did not support
+    // Preact 10. In future we should unify them. This will be a breaking change
+    // as it will affect the output for either Preact v8 or Preact v10.
     if (isPreact10()) {
-      // preact-render-to-string does not support Preact 10 yet.
       const tempContainer = document.createElement('div');
       render(el as any, tempContainer);
       const html = tempContainer.innerHTML;
