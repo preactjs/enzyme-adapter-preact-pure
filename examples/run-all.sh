66532ab3d8e725f140999a79cf3ba43f334c1954
Robert Knight <robertknight@gmail.com>
2019-11-09 12:10:37 +0000

    Use local version of package to test examples
    Use the version of the adapter package from the current git checkout to
    run the examples, instead of a version installed from npm.
    
    This means that CI builds will verify that the version of the package in
    the current tree works with the example, not just the version published
    to npm.
    
    It also avoids a problem when examples depend on changes in the adapter
    package that have not yet been published to npm.

--- examples/run-all.sh
+++ examples/run-all.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-base_dir=$(dirname $0)
+set -eu
+
+base_dir=$(realpath $(dirname $0))
+package_dir="$base_dir/.."
+yalc_bin="$package_dir/node_modules/.bin/yalc"
+
+# Build and "publish" the package locally.
+(cd $package_dir && $yalc_bin publish)
+
+# Run all the example projects.
 for file in $(ls $base_dir)
 do
   if [ -d "$base_dir/$file" ]
   then
     echo "Running example \"$base_dir/$file\""
-    (cd "$base_dir/$file" && rm -rf node_modules/ && npm ci && npm test)
+
+    $base_dir/run-one.sh $base_dir/$file
   fi
 done
