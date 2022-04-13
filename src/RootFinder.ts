fc725a1865ae3991b9465a7c72a1329cca6ce580
Robert Knight <robertknight@gmail.com>
2022-04-13 11:06:58 +0100

    Refactor `wrapWithWrappingComponent` implementation
    Apply some general simplifications and documentation improvements to
    this code. This will help addressing some issues related to React v18
    types amongst other things.
    
     - Remove unused indirection over the `createElement` call. This
       indirection existed within Enzyme to support reusing the
       `wrapWithWrappingComponent` utility with different versions of the
       React adapter. We don't need that here.
    
     - Use the `ReactElement` type from React only at the boundary with the
       public API of the package. Internal code uses the `VNode` type from
       Preact. This makes it easier to debug/address issues with conflicts
       between these types.
    
     - Use the term "element" (or "el") to refer to `ReactElement`s rather
       than `node`, to avoid confusion with use of `node` to refer to
       `RSTNode` objects elsewhere.
    
     - Make RootFinder a functional component, as that is preferred over the
       class API where possible, and document what it is for

--- src/RootFinder.ts
+++ src/RootFinder.ts
@@ -1,7 +1,13 @@
-import { Component } from 'preact';
+import type { ComponentChildren, VNode } from 'preact';
 
-export default class RootFinder extends Component {
-  render() {
-    return this.props.children;
-  }
+/**
+ * Helper for the `wrappingComponent` option for shallow rendering which is
+ * used to locate the wrapped content in the output of a wrapper component.
+ */
+export default function RootFinder({
+  children,
+}: {
+  children: ComponentChildren;
+}) {
+  return children as VNode;
 }
