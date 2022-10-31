22892176bf6c24c1b52bda030d6b29a309a10f0f
Andre Wiggins <459878+andrewiggins@users.noreply.github.com>
2022-10-31 08:32:43 -0700

    Add husky and lint-staged flow for formatting (#212)
    * Add husky and lint-staged flow
    
    * Reformat all files

--- examples/counter/test/Counter-test.js
+++ examples/counter/test/Counter-test.js
@@ -6,12 +6,12 @@ import Counter from '../src/Counter';
 
 describe('Counter', () => {
   it('should display initial count', () => {
-    const wrapper = mount(<Counter initialCount={5}/>);
+    const wrapper = mount(<Counter initialCount={5} />);
     expect(wrapper.text()).to.include('Current value: 5');
   });
 
   it('should increment after "Increment" button is clicked', () => {
-    const wrapper = mount(<Counter initialCount={5}/>);
+    const wrapper = mount(<Counter initialCount={5} />);
 
     wrapper.find('button').simulate('click');
 
