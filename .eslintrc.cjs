6c7ab8551739d17831d3c54ae7e5cd9a98cc097c
Andre Wiggins <andrewiggins@live.com>
2022-11-04 10:32:42 -0700

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    "@typescript-eslint/consistent-type-imports": "warn"
+  }
+};
