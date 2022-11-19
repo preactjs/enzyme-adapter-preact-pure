927ea0cfdfa7fe91416a8c5719af506e80022ff2
Andre Wiggins <andrewiggins@live.com>
2022-11-18 18:42:37 -0800

    Improve simulateError in new shallow renderer

--- src/preact10-rst.ts
+++ src/preact10-rst.ts
@@ -135,7 +135,7 @@ export function rstNodeFromElement(node: VNode | null | string): RSTNodeTypes {
     props,
     key: node.key || null,
     ref,
-    instance: null,
+    instance: getComponent(node),
     rendered: children,
   };
 }
